d948014eace3520a2bf9f3076cd1ae3124a8d5e6
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2021-05-08 20:42:10 -0700

    retrieving the data from the json file to our web application
    using the tag each to loop through all the instances in the json file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+const pizza_builder = require('../pizza_builder.json');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bongiorno Pizzeria' });
+  res.render('index', { title: 'Bongiorno Pizzeria',  pizza_builder});
 });
 
 module.exports = router;
